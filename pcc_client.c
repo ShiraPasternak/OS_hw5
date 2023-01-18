5cef5074cbc11ef487aa3d8e521819ec418d089d
shira pasternak <shirap1@mail.tau.ac.il>
2023-01-18 23:52:17 +0200

    final version of client with comments

--- pcc_client.c
+++ pcc_client.c
@@ -17,9 +17,6 @@
 #include <string.h>
 
 #define MB (1024 * 1024)
-//#define MB 10
-
-// todo adding perror and exit(1) where needed
 
 int writeBufferToServer(int sockfd, char *buff, size_t messageLen);
 int writeIntToServer(int fd, long int num);
@@ -50,6 +47,8 @@ int main(int argc, char **argv) { //general build taken from recitations code
         perror("Can't open file for input path\n");
         exit(1);
     }
+
+    // Calculate size of file
     if (fseek(fd, 0, SEEK_END) < 0) {
         perror("error in fseek in file");
         cleanUp(fd, sockfd);
@@ -61,16 +60,16 @@ int main(int argc, char **argv) { //general build taken from recitations code
         cleanUp(fd, sockfd);
         exit(1);
     }
-    //rewind(fd);
+
     if (fseek(fd, 0, SEEK_SET) < 0) {
         perror("error in fseek in file");
         cleanUp(fd, sockfd);
         exit(1);
     }
-    //printf("lenOfFile = %lu\n", (unsigned long)lenOfFile);
 
+    // Build server address
     memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET; // todo make sure if needed
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     int success = inet_pton(AF_INET, ip, &serv_addr.sin_addr);
     if (success == 0) {
@@ -90,42 +89,37 @@ int main(int argc, char **argv) { //general build taken from recitations code
         exit(1);
     }
 
-    // connect socket to the target address
     if(connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("Connect Failed");
         cleanUp(fd, sockfd);
         exit(1);
     }
 
+    // Write N (size of file) to server
     if (writeIntToServer(sockfd, lenOfFile) < 0) {
         cleanUp(fd, sockfd);
         exit(1);
     }
-    /*uint32_t file_len_network_byte_order = htonl((uint32_t) lenOfFile); // todo handle error
-    if (send(sockfd, &file_len_network_byte_order, sizeof(uint32_t), 0) != sizeof(uint32_t)) { // https://www.gta.ufrj.br/ensino/eel878/sockets/htonsman.html
-        perror("failed to send len of file to server");
-    }*/
 
-    //memset(fileBuff, 0,sizeof(MB));
-
+    // Read from file in chunks of 1 MB or less and write them to server
     int charRead = 0, remChar, chunks = 0, expChunkLen;
     remChar = lenOfFile;
     if (lenOfFile % MB > 0)
         chunks = (lenOfFile / MB) + 1;
     else
         chunks = lenOfFile / MB;
-    for (int i = 0; i < chunks; ++i) { // writing in chunks of 1 MB
+    for (int i = 0; i < chunks; ++i) {
         if (remChar / MB == 0)
             expChunkLen = remChar % MB;
         else
             expChunkLen = MB;
-        charRead = fread(fileBuff, 1, expChunkLen, fd);
-        if (charRead < expChunkLen) {
+        charRead = fread(fileBuff, 1, expChunkLen, fd); // Read chunk from file
+        if (charRead < expChunkLen) { // fread() didn't read the expected amount of chars
             perror("Failed reading file");
             cleanUp(fd, sockfd);
             exit(1);
         }
-        else {
+        else { // Write chunk to server
             if (writeBufferToServer(sockfd, fileBuff, expChunkLen) < 0) {
                 cleanUp(fd, sockfd);
                 exit(1);
@@ -134,47 +128,23 @@ int main(int argc, char **argv) { //general build taken from recitations code
             clearBuffer(fileBuff);
         }
     }
-    /*    char c;
-    int charCounter = 0, chunksCounter = 0;
-    while ((c = fgetc(fd)) != EOF) {
-        fileBuff[charCounter] = c;
-        charCounter++;
-        if (charCounter==MB) {
-            if (writeBufferToServer(sockfd, fileBuff, MB) < 0) {
-                cleanUp(fd, sockfd);
-                exit(1);
-            }
-            clearBuffer(fileBuff);
-            chunksCounter++;
-            charCounter = 0;
-            printf("charCounter = %d, chunksCounter = %d\n", charCounter);
-        }
-    }
-    if (c == EOF && (((chunksCounter*MB) + charCounter) != lenOfFile)) {
-        perror("Failed reading file");
-        cleanUp(fd, sockfd);
-        exit(1);
-    }
-    if (charCounter != 0) {
-        printf("shit im here1\n");
-        if (writeBufferToServer(sockfd, fileBuff, charCounter) < 0) {
-            cleanUp(fd, sockfd);
-            exit(1);
-        }
-    }
-    printf("shit im here2\n");*/
+
+    // Read C (number of printable) from server
     uint32_t numPrintableChars = readIntFromServer(sockfd);
     if (numPrintableChars < 0) {
         cleanUp(fd, sockfd);
         exit(1);
     }
-    /*
-    if (recv(sockfd, &num_of_printable_chars, sizeof(uint32_t), 0) != sizeof(uint32_t)) {
-        perror("failed to read num_of_printable_chars from server");
-    }*/
+
     printf("# of printable characters: %u\n", (unsigned int) numPrintableChars);
     cleanUp(fd, sockfd);
     exit(0);
+}
+
+void clearBuffer(char *buff) {
+    for (int i = 0; i < MB; ++i) {
+        buff[i] = 0;
+    }
 }
 
 void cleanUp(FILE *fd, int sockfd) {
@@ -192,6 +162,12 @@ uint32_t readIntFromServer(int sockfd) { //https://stackoverflow.com/questions/9
     return ntohl(recv);
 }
 
+int writeIntToServer(int sockfd, long int num) { // https://stackoverflow.com/questions/9140409/transfer-integer-over-a-socket-in-c
+    uint32_t conv = htonl(num);
+    char *dataBuff = (char*)&conv;
+    return writeBufferToServer(sockfd, dataBuff, sizeof(conv));
+}
+
 int readToBuffFromServer(int sockfd, char *buff, size_t messageLen) {
     int charRead = 0;
     int totalRead = 0;
@@ -201,20 +177,12 @@ int readToBuffFromServer(int sockfd, char *buff, size_t messageLen) {
             perror("error while reading from server to client");
             return -1;
         }
-        //buff[bytes_read] = '\0';
         totalRead += charRead;
     }
     return 0;
 }
 
-int writeIntToServer(int sockfd, long int num) { // https://stackoverflow.com/questions/9140409/transfer-integer-over-a-socket-in-c
-    uint32_t conv = htonl(num);
-    char *dataBuff = (char*)&conv;
-    return writeBufferToServer(sockfd, dataBuff, sizeof(conv));
-}
-
 int writeBufferToServer(int sockfd, char *buff, size_t messageLen) {
-    //printf("in writeBufferToServer with len = %d and shift = %d\n", (int)messageLen, shifting);
     int charSend = 0;
     int totalSent = 0;
     while (messageLen - totalSent > 0) {
@@ -226,10 +194,4 @@ int writeBufferToServer(int sockfd, char *buff, size_t messageLen) {
         totalSent += charSend;
     }
     return 0;
-}
-
-void clearBuffer(char *buff) {
-    for (int i = 0; i < MB; ++i) {
-        buff[i] = 0;
-    }
 }
